5161f3e8f4e1f888324864040d306b9d3ff1a14e
David Sypien <davidsypien@gmail.com>
2016-01-15 06:49:23 -0500

    Adding activity component

---
+++ app/components/activity.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "angular2/core";
+import {RouterOutlet} from "angular2/router";
+
+@Component({
+	selector: 'activity',
+	directives: [RouterOutlet],
+	templateUrl: '/app/components/activity.component.html'
+})
+
+export class Activity{
+	
+}
