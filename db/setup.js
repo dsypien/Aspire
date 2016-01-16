c1694cefb924f5f0f00dd63f011b54c25de8ca4e
David Sypien <davidsypien@gmail.com>
2016-01-16 06:36:55 -0500

    Extract db functionality into its own module

---
+++ db/setup.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+
+module.exports = function(connectionUrl, passport){
+	mongoose.connect(connectionUrl); // connect to our database
+
+	require('../config/passport')(passport);
+
+	mongoose.connection.on('connected', function () {  
+	  console.log('Mongoose default connection open to ' + connectionUrl);
+	}); 
+
+	// If the connection throws an error
+	mongoose.connection.on('error',function (err) {  
+	  console.log('Mongoose default connection error: ' + err);
+	}); 
+
+	// When the connection is disconnected
+	mongoose.connection.on('disconnected', function () {  
+	  console.log('Mongoose default connection disconnected'); 
+	});
+
+	// If the Node process ends, close the Mongoose connection 
+	process.on('SIGINT', function() {  
+	  mongoose.connection.close(function () { 
+	    console.log('Mongoose default connection disconnected through app termination'); 
+	    process.exit(0); 
+	  }); 
+	}); 
+
+	return mongoose;
+}
