92e992ac386bb543a34e7c4316754ca23e236da2
David Sypien <davidsypien@gmail.com>
2016-02-06 08:55:11 -0500

    Add notes component

---
+++ app/components/note.component.ts
@@ -0,0 +1,12 @@
+import {Component, Injectable} from 'angular2/core';
+
+@Component({
+	selector: "note",
+	templateUrl: "/app/components/note.component.html"
+})
+
+@Injectable()
+
+export class NoteComponent{
+
+}
