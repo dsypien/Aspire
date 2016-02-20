fe909a925dd217aabe01888cf2bf65f8ee40114e
David Sypien <davidsypien@gmail.com>
2016-02-20 11:29:40 -0500

    replace old backend code with authenticated api from aspire-api

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-	url: "mongodb://AspireService:YeAhBo11111a5pir333@ds041140.mongolab.com:41140/aspire"
+	'secret': 	'z7Xd2Wb%3@cM90wp*dX@98Po@1^dMn14n2',
+	'database': 'mongodb://AspireService:YeAhBo11111a5pir333@ds041140.mongolab.com:41140/aspire'
 };
--- server.js
+++ server.js
@@ -1,81 +1,38 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-
 var app = express();
-var flash  = require('connect-flash');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var path = require('path');
 
-var authRouter = require('./routes/auth');
+var config = require('./config/database');
 
-// DB setup
-var dbConfigUrl = require('./config/database.js').url;
-var dbSetup = require('./db/setup.js');
-var db = dbSetup(dbConfigUrl, app);
+var indexRoute = require('./routes/index');
+var ngAppRoute = require('./routes/ng-app');
+var authenticateRoute = require('./routes/authenticate');
+var registerRoute = require('./routes/register');
+var loginRoute = require('./routes/login');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Config
+var port = process.env.PORT || 3000;
+mongoose.connect(config.database);
+app.set('superSecret', config.secret);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
-app.use('/node_modules',  express.static(__dirname + '/node_modules'));
+
+app.use(morgan('dev'));
+
+//routes
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/login', loginRoute);
+app.use('/', ngAppRoute);
+//app.use('/dep/', ngAppRoute);
 
-// required for passport
-app.use(session({ secret: 'z7Xd2Wb%3@cM90wp*dX@98Po@1^dMn14n2'})); // session secret
-require('./config/passport.js')(app);
-app.use(flash()); // use connect-flash for flash messages stored in session
-
-// Routes
-//require('./app-routes.js')(app, passport);
-
-app.use('/auth', authRouter);
-
-app.get('*', function(req, res, next){
-   res.sendFile('index.html', { root: __dirname + "/public" });
-});
+app.use(authenticateRoute);// Authenticate all routes after this
+app.use('/', indexRoute);
+app.use('/register', registerRoute);
 
 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
+app.listen(port);
+console.log('Server running on port ' + port);
