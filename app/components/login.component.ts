c0dd1f43ccfa3912ab156f2b6dc0131da25e33bd
David Sypien <davidsypien@gmail.com>
2016-02-24 07:31:44 -0500

    updating login form to use ng directives

--- app/components/login.component.ts
+++ app/components/login.component.ts
@@ -1,13 +1,19 @@
 import {Component} from 'angular2/core';
 import {NgForm} from 'angular2/common';
 import {RouterOutlet} from 'angular2/router';
+import {AuthService} from '../services/auth.service';
 
 @Component({
 	selector: 'login-form',
-	directives: [RouterOutlet],
+	directives: [RouterOutlet, NgForm],
+	providers: [AuthService],
 	templateUrl: '/app/components/login.component.html'
 })
 
 export class LoginComponent{
+	constructor(private _authService: AuthService){}
 
+	authenticate(data){
+		console.log(data);
+	}
 }
